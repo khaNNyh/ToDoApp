2194c0c600c0d8fbd054196e962ba76bf59aeca6
kaNNy <kanyqan@gmail.com>
2022-11-23 20:54:10 +0100

    dodawanie tasków

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 import { NewListComponent } from './pages/new-list/new-list.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, TaskViewComponent, NewListComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    NoopAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/new-list/new-list.component.ts
+++ src/app/pages/new-list/new-list.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-new-list',
   templateUrl: './new-list.component.html',
-  styleUrls: ['./new-list.component.scss']
+  styleUrls: ['./new-list.component.scss'],
 })
 export class NewListComponent implements OnInit {
+  constructor(private http: HttpClient, private _formBuilder: FormBuilder) {}
 
-  constructor() { }
+  newListName = new FormControl('', [Validators.required]);
+
+  Lists!: any;
 
   ngOnInit(): void {
+    this.http
+      .get('http://localhost:3000/lists')
+      .subscribe((response) => (this.Lists = response));
   }
 
+  addNewList() {
+    const newListData = {
+      id: this.Lists.length + 1,
+      name: this.newListName.value,
+    };
+    this.http.post('http://localhost:3000/lists', newListData).subscribe();
+  }
 }
