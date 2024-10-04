9054f0d0f76003475eef090a8e6dd3862d602c1e
k.klimas <kanyqan@gmail.com>
2024-10-04 21:18:00 +0200

    ToDoApp - supressing error

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -3,7 +3,7 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', true);
 mongoose
   .connect("mongodb://127.0.0.1:27017/TaskManager", {
     useNewUrlParser: true,
