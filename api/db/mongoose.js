11222ab8a314ad4ef21e4a3c1425d9532696702c
k.klimas <kanyqan@gmail.com>
2024-10-04 19:46:19 +0200

    ToDoApp - package update, removed error

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -3,6 +3,7 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
+mongoose.set('strictQuery', false);
 mongoose
   .connect("mongodb://127.0.0.1:27017/TaskManager", {
     useNewUrlParser: true,
